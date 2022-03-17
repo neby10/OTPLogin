7928f4e14c04f047aa57c22fbc6f41cc05300591
Nicholas Eby <49201079+neby10@users.noreply.github.com>
2022-03-17 15:03:05 -0400

    Generating Random OTP
    -does not use SMS messaging OTP
    -just generates random OTP and then displays "success" if code submitted is the same code

--- index.js
+++ index.js
@@ -1,20 +1,33 @@
-// console.log("hello there");
+// March 17, 2022
 
 
+var otp_password;
 
+document.getElementById("generate-submit").addEventListener('click', function(e) {
+    // e.preventDefault();
+    console.log("generate clicked");
 
+    otp_password = Math.floor(Math.random() * 10000).toString();
+    let otp = document.getElementById("otp-password");
+    otp.innerHTML = otp_password;
+})
 
-function generate() {
-    console.log("generate function was called");
-}
+document.getElementById("login-submit").addEventListener('click', function(e) {
+    // e.preventDefault();
+    console.log("button clicked");
 
-function login() {
     let result = document.getElementById("result");
     result.style.display = "block";
     let password = document.getElementById("password");
-    if (password.value === "0000") {
+    if (password.value === otp_password) {
         result.innerHTML = "SUCCESS";
     } else {
         result.innerHTML = "TRY AGAIN";
     }
-}
+})
+
+// document.addEventListener("keydown", function(e) {
+//     if (e.key === 'Enter') {
+//         alert('Enter is pressed!');
+//     }
+// })
